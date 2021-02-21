36e96b8e646a30a3c17c5809132edf6909f09e14
AdwinKing <jin2000112014@gmail.com>
2021-02-21 13:55:44 +0800

    use ctrl-e for up-or-search in fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,4 +6,6 @@ thefuck --alias | source
 bind -M insert \cf forward-char
 bind -M insert \cg forward-word
 bind -M insert -m default jj backward-char force-repaint
+bind -M insert \ce up-or-search
+
 
